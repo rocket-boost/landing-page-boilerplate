84fb9d5a8076c64cb6b888b82e0a54648ff01920
Mladen Ilic <mladenilic480@gmail.com>
2019-08-02 21:09:08 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve('./src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'js/scripts.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/html/index.html')
+    }),
+    new MiniCssExtractPlugin({ filename: 'css/styles.css' }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, './src/assets'), to: 'assets' }
+    ]),
+  ],
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+        test: /\.s?css/i,
+        use : [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+};
